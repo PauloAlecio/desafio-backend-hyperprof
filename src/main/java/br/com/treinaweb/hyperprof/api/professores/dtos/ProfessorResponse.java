c5abd3fd66b10549f137038fec9d902df10fdc94
PauloAlecio <46734970+PauloAlecio@users.noreply.github.com>
2023-12-28 23:50:14 -0300

    Cria DTOs e Mappers de professor

---
+++ src/main/java/br/com/treinaweb/hyperprof/api/professores/dtos/ProfessorResponse.java
@@ -0,0 +1,28 @@
+package br.com.treinaweb.hyperprof.api.professores.dtos;
+
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProfessorResponse {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private String descricao;
+    private BigDecimal valorHora;
+    private String fotoPerfil;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
