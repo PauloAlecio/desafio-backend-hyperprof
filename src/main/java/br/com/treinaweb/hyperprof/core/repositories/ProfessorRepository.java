521e11b21673a37f69ea46031cdb593a63f2ba20
PauloAlecio <46734970+PauloAlecio@users.noreply.github.com>
2023-12-29 00:02:50 -0300

    Adiciona metodo de busca por descrição

--- src/main/java/br/com/treinaweb/hyperprof/core/repositories/ProfessorRepository.java
+++ src/main/java/br/com/treinaweb/hyperprof/core/repositories/ProfessorRepository.java
@@ -3,6 +3,9 @@ package br.com.treinaweb.hyperprof.core.repositories;
 import br.com.treinaweb.hyperprof.core.models.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProfessorRepository extends JpaRepository<Professor,Long>
 {
+    List<Professor> findByDescricaoContaining(String descricao);
 }
