a5f508a9f2ade00899e5812d2ec4ac675b80d5b4
PauloAlecio <46734970+PauloAlecio@users.noreply.github.com>
2023-12-29 00:43:38 -0300

    Adiciona rota busca por id

--- src/main/java/br/com/treinaweb/hyperprof/api/commons/routes/ApiRoutes.java
+++ src/main/java/br/com/treinaweb/hyperprof/api/commons/routes/ApiRoutes.java
@@ -13,5 +13,7 @@ public class ApiRoutes {
 
     public static final String BUSCAR_PROFESSORES = API + PROFESSORES;
 
+    public static final String BUSCAR_PROFESSOR_POR_ID = API + PROFESSORES + "/{professorId}";
+
 
 }
