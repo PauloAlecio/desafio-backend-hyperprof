6162bccbabf433aaca714a854ebed149594b1f36
PauloAlecio <46734970+PauloAlecio@users.noreply.github.com>
2024-01-02 19:15:12 -0300

    Cria o Serviço de Authenticação

---
+++ src/main/java/br/com/treinaweb/hyperprof/core/services/authentication/AuthenticationService.java
@@ -0,0 +1,26 @@
+package br.com.treinaweb.hyperprof.core.services.authentication;
+
+import br.com.treinaweb.hyperprof.core.models.AuthenticatedUser;
+import br.com.treinaweb.hyperprof.core.repositories.ProfessorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService implements
+    UserDetailsService
+{
+
+    private final ProfessorRepository professorRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return professorRepository.findByEmail(username)
+            .map(AuthenticatedUser::new)
+            .orElseThrow(() -> new UsernameNotFoundException("Credenciais inválidas"));
+    }
+
+}
