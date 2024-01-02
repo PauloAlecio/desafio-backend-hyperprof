b02bd4926da5ea87651ffbcef3a0f8b53dfe4226
PauloAlecio <46734970+PauloAlecio@users.noreply.github.com>
2024-01-02 16:24:31 -0300

    Cria método que verifica exixtencia de email na base de dados

--- src/main/java/br/com/treinaweb/hyperprof/core/repositories/ProfessorRepository.java
+++ src/main/java/br/com/treinaweb/hyperprof/core/repositories/ProfessorRepository.java
@@ -4,8 +4,10 @@ import br.com.treinaweb.hyperprof.core.models.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProfessorRepository extends JpaRepository<Professor,Long>
 {
     List<Professor> findByDescricaoContaining(String descricao);
+    boolean existsByEmail(String email);
 }
