e5f75bcd404d4a72a50f93388c0b69299389859e
PauloAlecio <46734970+PauloAlecio@users.noreply.github.com>
2024-01-09 14:56:50 -0300

    Implementa os principais métodos da interface TokenService

--- src/main/java/br/com/treinaweb/hyperprof/core/services/token/providers/jjwt/JjwtTokenService.java
+++ src/main/java/br/com/treinaweb/hyperprof/core/services/token/providers/jjwt/JjwtTokenService.java
@@ -1,8 +1,18 @@
 package br.com.treinaweb.hyperprof.core.services.token.providers.jjwt;
 
+import br.com.treinaweb.hyperprof.core.repositories.TokenInvalidoRepository;
 import br.com.treinaweb.hyperprof.core.services.token.TokenService;
+import br.com.treinaweb.hyperprof.core.services.token.TokenServiceException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.time.Instant;
+import java.util.HashMap;
 
 @Service
 @RequiredArgsConstructor
@@ -10,8 +20,12 @@ public class JjwtTokenService implements TokenService {
 
     private final JjwtConfigProperties configProperties;
     @Override
-    public String gerarAccessToken ( String subject ) {
-        return null;
+    public String gerarAccessToken(String subject) {
+        return gerarToken(
+            subject,
+            configProperties.getAccessTokenExpirationInSeconds(),
+            configProperties.getAccessTokenSigingKey()
+        );
     }
 
     @Override
@@ -20,17 +34,53 @@ public class JjwtTokenService implements TokenService {
     }
 
     @Override
-    public String gerarRefreshToken ( String subject ) {
-        return null;
+    public String gerarRefreshToken(String subject) {
+        return gerarToken(
+            subject,
+            configProperties.getRefreshTokenExpirationInSeconds(),
+            configProperties.getRefreshTokenSigingKey()
+        );
     }
 
     @Override
-    public String getSubjectDoRefreshToken ( String refreshToken ) {
-        return null;
+    public String getSubjectDoRefreshToken(String refreshToken) {
+        return getClaims(refreshToken, configProperties.getRefreshTokenSigingKey()).getSubject();
     }
+
 
     @Override
     public void invalidarTokens ( String... tokens ) {
 
+    }
+
+    private String gerarToken(String subject, Long expirationInSeconds, String siginKey) {
+        var dataHoraAtual = Instant.now();
+        var dataHoraExpiracao = dataHoraAtual.plusSeconds(expirationInSeconds);
+        return Jwts.builder()
+            .setClaims(new HashMap<String, Object>())
+            .setSubject(subject)
+            .setIssuedAt(Date.from(dataHoraAtual))
+            .setExpiration(Date.from(dataHoraExpiracao))
+            .signWith(Keys.hmacShaKeyFor(siginKey.getBytes()))
+            .compact();
+    }
+
+
+    private Claims getClaims(String token, String sigingKey) {
+        try {
+            return tryGetClaims(token, sigingKey);
+        } catch (JwtException e) {
+            throw new TokenServiceException(e.getLocalizedMessage());
+        }
+    }
+
+
+    private Claims tryGetClaims(String token, String sigingKey) {
+
+        return Jwts.parserBuilder()
+            .setSigningKey(Keys.hmacShaKeyFor(sigingKey.getBytes()))
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
     }
 }
