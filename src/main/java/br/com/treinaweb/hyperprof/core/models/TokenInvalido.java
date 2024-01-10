b3b0d20ef8afa776f23c07d570c13088b5c4daf3
PauloAlecio <46734970+PauloAlecio@users.noreply.github.com>
2024-01-10 18:29:59 -0300

    Cria modelo de token inválido

---
+++ src/main/java/br/com/treinaweb/hyperprof/core/models/TokenInvalido.java
@@ -0,0 +1,25 @@
+package br.com.treinaweb.hyperprof.core.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tokens_invalidos")
+@ToString(onlyExplicitlyIncluded = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
+public class TokenInvalido extends Auditable {
+
+    @Id
+    @ToString.Include
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "token")
+    private String token;
+
+}
