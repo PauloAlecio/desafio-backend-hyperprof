cc68a2ec9c65b1fb9ae277c9b6a1fd2880f7d0cc
PauloAlecio <46734970+PauloAlecio@users.noreply.github.com>
2024-01-10 11:59:04 -0300

    Cria a interface MeService

---
+++ src/main/java/br/com/treinaweb/hyperprof/api/professores/services/MeService.java
@@ -0,0 +1,8 @@
+package br.com.treinaweb.hyperprof.api.professores.services;
+
+import br.com.treinaweb.hyperprof.api.professores.dtos.ProfessorResponse;
+
+public interface MeService
+{
+    ProfessorResponse buscarProfessorLogado();
+}
