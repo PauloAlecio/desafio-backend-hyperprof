07a7927e0e963573d9e87f1f545d2f004ec6340a
PauloAlecio <46734970+PauloAlecio@users.noreply.github.com>
2024-01-10 20:44:19 -0300

    Atualiza validaçao de e-mail

--- src/main/java/br/com/treinaweb/hyperprof/api/professores/validators/ProfessorEmailIsUniqueValidator.java
+++ src/main/java/br/com/treinaweb/hyperprof/api/professores/validators/ProfessorEmailIsUniqueValidator.java
@@ -4,6 +4,8 @@ import br.com.treinaweb.hyperprof.core.repositories.ProfessorRepository;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -20,7 +22,15 @@ public class ProfessorEmailIsUniqueValidator implements
         if (value == null) {
             return true;
         }
-
+        var authentication = SecurityContextHolder
+            .getContext().getAuthentication();
+        var isAuthenticated = authentication != null
+            && !(authentication instanceof AnonymousAuthenticationToken)
+            && authentication.isAuthenticated();
+        if (isAuthenticated) {
+            var professor = professorRepository.findByEmail(value);
+            return professor.isEmpty() || professor.get().getEmail().equals(authentication.getName());
+        }
         return !professorRepository.existsByEmail(value);
     }
 
