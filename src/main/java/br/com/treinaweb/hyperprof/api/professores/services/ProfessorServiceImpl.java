5c51d6ce3c7704341931a710eadb4de552628531
PauloAlecio <46734970+PauloAlecio@users.noreply.github.com>
2024-01-10 20:21:14 -0300

    Implementa método para atualizar professor logado

--- src/main/java/br/com/treinaweb/hyperprof/api/professores/services/ProfessorServiceImpl.java
+++ src/main/java/br/com/treinaweb/hyperprof/api/professores/services/ProfessorServiceImpl.java
@@ -4,8 +4,11 @@ import br.com.treinaweb.hyperprof.api.professores.dtos.ProfessorRequest;
 import br.com.treinaweb.hyperprof.api.professores.dtos.ProfessorResponse;
 import br.com.treinaweb.hyperprof.api.professores.mappers.ProfessorMapper;
 import br.com.treinaweb.hyperprof.core.exceptions.ProfessorNotFoundException;
+import br.com.treinaweb.hyperprof.core.models.AuthenticatedUser;
 import br.com.treinaweb.hyperprof.core.repositories.ProfessorRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -40,5 +43,16 @@ public class ProfessorServiceImpl implements ProfessorService
         professorParaCadastrar.setPassword(passwordEncoder.encode(professorParaCadastrar.getPassword()));
         var professorCadastrado = professorRepository.save(professorParaCadastrar);
         return professorMapper.toProfessorResponse(professorCadastrado);
+    }
+
+    @Override
+    public ProfessorResponse atualizarProfessorLogado(ProfessorRequest professorRequest) {
+        var authentication = SecurityContextHolder
+            .getContext().getAuthentication();
+        var professor = ((AuthenticatedUser) authentication.getPrincipal()).getProfessor();
+        BeanUtils.copyProperties(professorRequest, professor, "id", "password", "createdAt", "updatedAt");
+        professor.setPassword(passwordEncoder.encode(professorRequest.getPassword()));
+        var professorAtualizado = professorRepository.save(professor);
+        return professorMapper.toProfessorResponse(professorAtualizado);
     }
 }
